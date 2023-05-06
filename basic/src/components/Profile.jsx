279a5c03b0236a839accb602ddc0d1ca3233c971
usinia <usinia0103@gmail.com>
2023-05-06 19:07:09 +0900

    [#2] 4.11

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,7 +1,8 @@
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
       <img className="photo" src={image} alt="avatar" />
+      {isNew && <span className="new">NEW</span>}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
