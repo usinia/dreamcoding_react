27b78760ab8b3b7147cf52502656d2850aae62d0
usinia <usinia0103@gmail.com>
2023-05-07 17:51:51 +0900

    [#3] 5.23

--- basic-deep-dive/src/components/Products.jsx
+++ basic-deep-dive/src/components/Products.jsx
@@ -1,31 +1,10 @@
 import React, { useEffect, useState } from "react";
+import useProducts from "../hooks/use-products";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
-
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-
-    fetch(`data/${checked ? "sale_" : ""}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("🔥 뜨끈한 데이터를 네트워크에서 받아옴");
-        setProducts(data);
-      })
-      .catch((e) => setError("에러 발생!"))
-      .finally(() => setLoading(false));
-
-    return () => {
-      console.log("🧹 깨끗하게 청소하는 일을 합니다.");
-    };
-  }, [checked]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
@@ -49,7 +28,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
