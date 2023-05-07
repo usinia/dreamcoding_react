27b84ae9440db6a93b1d7f62012ed8f23211e671
usinia <usinia0103@gmail.com>
2023-05-07 15:07:49 +0900

    [#3] 5.6

--- basic-deep-dive/src/index.js
+++ basic-deep-dive/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppXY from "./AppXY";
 import reportWebVitals from "./reportWebVitals";
+import AppMentor from "./AppMentor";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppXY />
+    <AppMentor />
   </React.StrictMode>
 );
 
