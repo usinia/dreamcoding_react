ada0e413d68e71e62d6ba9f8b549e9f4a8e65af7
usinia <usinia0103@gmail.com>
2023-05-14 18:48:16 +0900

    [#4] 6.5 Tailwind

--- css/src/App.js
+++ css/src/App.js
@@ -1,36 +1,16 @@
 import "./App.css";
-import Button1 from "./components/Button1";
-import Button2 from "./components/Button2";
-import styled, { css } from "styled-components";
-
-const Container = styled.div`
-  display: felx;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${(props) =>
-    props.primary &&
-    css`
-      background: #009cd5;
-      color: white;
-    `}
-`;
+import PostCssButton1 from "./components/PostCssButton1";
+import PostCssButton2 from "./components/PostCssButton2";
+import StyledComponent from "./components/StyledComponent";
+import TailwindComponent from "./components/TailwindComponent";
 
 function App() {
   return (
     <>
-      <Button1 />
-      <Button2 />
-      <Container>
-        <Button>Normal Button</Button>
-        <Button primary>Primary Button</Button>
-      </Container>
+      <PostCssButton1 />
+      <PostCssButton2 />
+      <StyledComponent />
+      <TailwindComponent />
     </>
   );
 }
